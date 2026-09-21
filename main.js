3382b63bb5f159245621cbe3c0b57258506a3370
Dein Name <deine-email@example.com>
2026-09-21 15:48:26 +0200

    Initial commit: fahrraeder.json und main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+function ladeFahrraeder(pfad) {
+    const rohdaten = fs.readFileSync(pfad, 'utf-8');
+    return JSON.parse(rohdaten);
+}
+
+const fahrraeder = ladeFahrraeder('fahrraeder.json');
+console.log(fahrraeder);
